15929909a03cf5a12dc65e1c148bbe983b8f2bf5
Callum Rogers <callumrogers@hotmail.co.uk>
2011-06-11 10:53:56 +0100

    Moved Stack to it's own module

--- Repent.hs
+++ Repent.hs
@@ -1,30 +1,11 @@
 import Data.List
 import Char
+import Stack
 
 data Expr = Num Integer | Var String | Array [Integer] | Matrix [[Integer]]
             deriving (Show, Eq)
 
-data Stack a = EmptyStack | Stack a (Stack a)
-            deriving (Show, Eq)
 type EStack = Stack Expr
-
-
-push :: Stack a -> a -> Stack a
-push s i = Stack i s
-
-pop :: Stack a -> (a, Stack a)
-pop EmptyStack = error("Cannot pop from the emptystack")
-pop (Stack i s) = (i,s)
-
-popN :: Stack a -> Int -> ([a], Stack a)
-popN s n = inner s n []
-    where inner s 0 as = (as, s)
-          inner s n as = let (i,t) = pop s in inner t (n-1) (as++[i])
-
-pop2into :: Stack a -> (a -> a -> a) -> Stack a
-pop2into s f = push u $ f i j
-    where (i,t) = pop s
-          (j,u) = pop t
 
 eval :: EStack -> Char -> EStack
 eval s c
@@ -38,7 +19,6 @@ eval s c
 
 run :: EStack -> String -> EStack
 run = foldl eval
-
 
 
 addE :: Expr -> Expr -> Expr
